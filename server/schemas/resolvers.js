df510f8fa020bf7375d157ea8c052a9d15765d6e
Da Kim <daeunkim127@gmail.com>
2022-08-01 23:21:18 -0400

    schemas fixed

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,10 +4,7 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
 
     Query: {
-
-        //get a user by username
         me: async (parent, args, context) => {
-
             if(context.user) {
                 const userData = await User.findOne({})
                 .select('-__v -password')
--- server/server.js
+++ server/server.js
@@ -4,22 +4,22 @@ const { authMiddleware } = require('./utils/auth');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const { typeDefs, resolvers } = require('./schemas');
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
-})
-server.applyMiddleware({app});
+});
+
+// server.applyMiddleware({app});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
